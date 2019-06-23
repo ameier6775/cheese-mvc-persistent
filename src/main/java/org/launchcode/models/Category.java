ce0c07c996ee36c9d76c8fcd2ce2e7d1e632e3a0
Alex Meier <ameier6775@gmail.com>
2019-06-22 20:12:35 -0500

    Finished up second part of the assignment

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,10 +1,9 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Entity
 public class Category {
@@ -17,22 +16,24 @@ public class Category {
     @Size(min=3, max=15)
     private String name;
 
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses;
+
     public Category () {
     }
 
-    public Category (String name) {
+    public Category (String name, int id, List<Cheese>cheeses) {
         this.name = name;
+        this.id = id;
+        this.cheeses = cheeses;
     }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public List<Cheese> getCheeses() { return cheeses; }
+    public void setCheeses(List<Cheese> cheeses) { this.cheeses = cheeses; }
 }
--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -3,6 +3,7 @@ package org.launchcode.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -24,40 +25,28 @@ public class Cheese {
     @Size(min=1, message = "Description must not be empty")
     private String description;
 
-    private CheeseType type;
+    @ManyToOne
+    private Category category;
 
-    public Cheese(String name, String description) {
+    public Cheese(String name, String description, Category category) {
         this.name = name;
         this.description = description;
+        this.category = category;
     }
 
     public Cheese() { }
 
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) { this.name = name; }
 
-    public String getDescription() {
-        return description;
-    }
+    public String getDescription() { return description; }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    public void setDescription(String description) { this.description = description; }
 
-    public CheeseType getType() {
-        return type;
-    }
+    public Category getCategory() { return category; }
 
-    public void setType(CheeseType type) {
-        this.type = type;
-    }
+    public void setCategory(Category category) { this.category = category; }
 }
